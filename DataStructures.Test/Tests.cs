9e4674f7972821bcdac0f04bb50932f31bc7df05
Kris Harper <kris.p.harper@gmail.com>
2015-04-06 18:42:05 -0400

    Moved everything into one directory

---
+++ DataStructures.Test/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Test
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void BinaryTreeTest()
+        {
+            BinaryTree<int> btree = new BinaryTree<int>();
+            btree.Root = new BinaryTreeNode<int>(1);
+            btree.Root.Left = new BinaryTreeNode<int>(2);
+            btree.Root.Right = new BinaryTreeNode<int>(2);
+        }
+    }
+}
