5f98bf532e8fbbe80776078751adf16e632baf0a
Kris Harper <kris.p.harper@gmail.com>
2015-04-06 23:12:12 -0400

    Added sort algorithms

---
+++ Data Structures/Sort.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public static class Sort
+    {
+        public static IEnumerable<T> MergeSort<T>(this IEnumerable<T> collection) where T : IComparable<T>
+        {
+            if (collection.Count() <= 1)
+            {
+                return collection;
+            }
+
+            //var left = collection.Where((e, i) => i <= collection.Count() / 2).MergeSort();
+            //var right = collection.Where((e, i) => i > collection.Count() / 2).MergeSort();
+            var left = collection.Where((e, i) => i < collection.Count() / 2).ToList();
+            left = left.MergeSort().ToList();
+            var right = collection.Where((e, i) => i >= collection.Count() / 2).ToList();
+            right = right.MergeSort().ToList();
+            return Merge(left, right);
+        }
+
+        private static IEnumerable<T> Merge<T>(IEnumerable<T> left, IEnumerable<T> right) where T : IComparable<T>
+        {
+            List<T> result = new List<T>();
+
+            while (left.Count() > 0 && right.Count() > 0)
+            {
+                if (left.First().CompareTo(right.First()) <= 0)
+                {
+                    result.Add(left.First());
+                    left = left.Where((e, i) => i > 0);
+                }
+                else
+                {
+                    result.Add(right.First());
+                    right = right.Where((e, i) => i > 0);
+                }
+            }
+
+            while (left.Count() > 0)
+            {
+                result.Add(left.First());
+                left = left.Where((e, i) => i > 0);
+            }
+
+            while (right.Count() > 0)
+            {
+                result.Add(right.First());
+                right = right.Where((e, i) => i > 0);
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<T> QuickSort<T>(this IEnumerable<T> collection) where T : IComparable<T>
+        {
+            if (collection.Count() <= 1)
+            {
+                return collection;
+            }
+
+            T pivot = collection.ElementAt(1);
+            IEnumerable<T> left = collection.Where(element => element.CompareTo(pivot) < 0);
+            IEnumerable<T> right = collection.Where(element => element.CompareTo(pivot) > 0);
+
+            return left.QuickSort().Concat(new List<T> { pivot }).Concat(right.QuickSort());
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+        {
+            Random rng = new Random();
+            T[] array = collection.ToArray();
+            int n = array.Count();
+
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+
+            foreach (T element in array)
+            {
+                yield return element;
+            }
+        }
+    }
+}
--- DataStructures.Test/Tests.cs
+++ DataStructures.Test/Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using DataStructures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,6 +13,29 @@ namespace DataStructures.Test
         public void BinaryTreeTest()
         {
             BinaryTree<int> btree = new BinaryTree<int>();
+        }
+
+        [TestMethod]
+        public void MergeSortTest()
+        {
+            Assert.IsTrue(Enumerable.Range(1, 100).Shuffle().MergeSort().SequenceEqual(Enumerable.Range(1, 100)));
+            Assert.IsTrue(Enumerable.SequenceEqual(Enumerable.Range(1, 1).Shuffle().MergeSort(), Enumerable.Range(1, 1)));
+            Assert.IsTrue(Enumerable.SequenceEqual(Enumerable.Empty<int>().MergeSort(), Enumerable.Empty<int>()));
+        }
+
+        [TestMethod]
+        public void QuickSortTest()
+        {
+            Assert.IsTrue(Enumerable.SequenceEqual(Enumerable.Range(1, 100).Shuffle().QuickSort(), Enumerable.Range(1, 100)));
+            Assert.IsTrue(Enumerable.SequenceEqual(Enumerable.Range(1, 1).Shuffle().QuickSort(), Enumerable.Range(1, 1)));
+            Assert.IsTrue(Enumerable.SequenceEqual(Enumerable.Empty<int>().QuickSort(), Enumerable.Empty<int>()));
+        }
+
+        [TestMethod]
+        public void ShuffleTest()
+        {
+            var shuffled = Enumerable.Range(1, 100).Shuffle();
+            Assert.IsFalse(Enumerable.SequenceEqual(shuffled, Enumerable.Range(1, 100)));
         }
     }
 }
